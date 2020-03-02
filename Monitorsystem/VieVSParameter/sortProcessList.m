67244f6bdf22d65fe910530082f22c91695c3606
tiegemccarthy <tiegem@utas.edu.au>
2020-03-02 15:59:36 +1100

    Added SEFD stuff and removed Jakob's files.

--- Monitorsystem/VieVSParameter/sortProcessList.m
+++
@@ -1,9 +0,0 @@
-function [ process_list] = sortProcessList( process_list )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-[~,numOfChars]=size(process_list);
-[ ~,~,index ] = mjdOfSession( process_list );
-process_list=process_list(index,1:numOfChars);
-
-end
-
